daa23ccd65cc6459226aeaf6f7d7896a889018be
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2023-10-05 01:01:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+$(document).ready(function () {
+  $(".image").draggable({
+    helper: "clone",
+    revert: "invalid",
+  });
+
+  $("#parent").droppable({
+    accept: ".image",
+    drop: function (event, ui) {
+      var sourceDiv = ui.helper[0];
+      var targetDiv = $(this)[0];
+
+      var sourceImage = $(sourceDiv).css("background-image");
+      var targetImage = $(targetDiv).css("background-image");
+      $(sourceDiv).css("background-image", targetImage);
+      $(targetDiv).css("background-image", sourceImage);
+    },
+  });
+});
